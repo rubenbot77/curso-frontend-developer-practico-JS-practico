2264b21efc12e154e967e040253bf6f8e364a750
rubenbot77 <mechasBRS707@hotmail.com>
2022-10-26 17:45:15 -0500

    agregando el menu del carrito de compras

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const menuCar = document.querySelector('.navbar-shopping-cart');
+const orderDetail = document.querySelector('.product-detail')
 
 navEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobileMenu);;
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCar.addEventListener('click', toggleShoppingCart);
 
 
 function toggleDesktopMenu(){
@@ -12,4 +15,7 @@ function toggleDesktopMenu(){
 }
 function toggleMobileMenu (){
     mobileMenu.classList.toggle('inactive');
+}
+function toggleShoppingCart() {
+    orderDetail.classList.toggle('inactive')
 }
