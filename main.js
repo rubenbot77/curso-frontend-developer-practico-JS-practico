d4acef58e81eb1239093d877709ff9552ecdb457
rubenbot77 <mechasBRS707@hotmail.com>
2022-10-26 18:29:34 -0500

    arreglando problemas de los menus

--- main.js
+++ main.js
@@ -11,11 +11,31 @@ menuCar.addEventListener('click', toggleShoppingCart);
 
 
 function toggleDesktopMenu(){
+    const isOrderDetailClosed = orderDetail.classList.contains('inactive');
+    if (!isOrderDetailClosed){
+        orderDetail.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive');
+
 }
 function toggleMobileMenu (){
+    const isOrderDetailClosed = orderDetail.classList.contains('inactive');
+
+    if (!isOrderDetailClosed){
+        orderDetail.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive');
 }
 function toggleShoppingCart() {
+    const ismobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+
+    if (!ismobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
     orderDetail.classList.toggle('inactive')
 }
